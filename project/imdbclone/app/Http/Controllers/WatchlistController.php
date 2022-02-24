8bf885b16a906c1768c05789521a6d51af230e9a
Nova Boman <nova.boman@chasacademy.se>
2022-02-24 15:18:20 +0100

    Add index and store functions

--- project/imdbclone/app/Http/Controllers/WatchlistController.php
+++ project/imdbclone/app/Http/Controllers/WatchlistController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Entry;
+use App\Models\Watchlist;
 use Illuminate\Http\Request;
 
 class WatchlistController extends Controller
 {
-    //
+    // Lägg till en lista
+    // Spara en lista i databasen med titel
+
+    // Ta bort en lista
+    // Ta bort lista på watchlist ID
+
+    // Visa en lista tillhörande user
+    // Visa lista med tillhörande entries 
+
+    public function index()
+    {
+        return view('watchlist.watchlist');
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'title' => 'string|required|max:50'
+        ]);
+
+        $watchlist = new Watchlist;
+        $watchlist->title = $request->title;
+        $watchlist->save();
+    }
 }
