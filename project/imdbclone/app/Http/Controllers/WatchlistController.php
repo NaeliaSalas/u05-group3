9296e16d71ac8d00e10d6528cf6153cb9555c556
Nova Boman <nova.boman@chasacademy.se>
2022-02-27 00:30:20 +0100

    Change WatchlistController to resource controller

--- project/imdbclone/app/Http/Controllers/WatchlistController.php
+++ project/imdbclone/app/Http/Controllers/WatchlistController.php
@@ -2,60 +2,83 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Entry;
-use App\Models\Watchlist;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class WatchlistController extends Controller
 {
-    // Lägg till en lista
-    // Spara en lista i databasen med titel 
-
-    // Ta bort en lista
-    // Ta bort lista på watchlist ID
-    //*****************
-
-    // Visa alla listor tillhörande user
-    // Visa lista med tillhörande entries 
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $userId = Auth::id();
-        $watchlists = Watchlist::where('user_id_fk', $userId)->get();
-        return view('watchlist.watchlist', ['watchlists' => $watchlists]);
+        //
     }
 
-    public function show($id)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-
-        $userId = Auth::id();
-        $watchlist = Watchlist::where('user_id_fk', $userId)
-            ->where('id', $id)->get();
-        return view('watchlist.show', ['watchlist' => $watchlist]);
+        //
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-
-        $request->validate([
-            'title' => 'string|required|max:50'
-        ]);
-
-        $watchlist = new Watchlist;
-        $watchlist->title = $request->title;
-        $watchlist->user_id_fk = $request->user_id_fk;
-        $watchlist->save();
-
-        return redirect('watchlist');
+        //
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
-
-        $watchlist = Watchlist::find($id);
-        $watchlist->delete();
-
-        return redirect('watchlist');
+        //
     }
 }
