431edbc7ad9cb430a9963270e61801b5aa4aee8f
Nova Boman <nova.boman@chasacademy.se>
2022-02-28 22:33:05 +0100

    Check if movie already exist in watchlist

--- project/imdbclone/app/Http/Controllers/EntryController.php
+++ project/imdbclone/app/Http/Controllers/EntryController.php
@@ -36,19 +36,24 @@ class EntryController extends Controller
     public function store(Request $request)
     {
 
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'movie_id_fk' => 'required',
-            'watchlist_id_fk' => 'required'
+        if (Entry::where('movie_id_fk', $request->movie_id_fk)->where('watchlist_id_fk', $request->watchlist_id_fk)->exists()) {
 
-        ]);
+            return redirect('watchlist')->with('mesage', 'Alredy exists in watchlist');
+        } else {
+            $request->validate([
+                'title' => 'required|string|max:255',
+                'movie_id_fk' => 'required',
+                'watchlist_id_fk' => 'required'
 
-        $entry = new Entry;
-        $entry->movie_id_fk = $request->movie_id_fk;
-        $entry->watchlist_id_fk = $request->watchlist_id_fk;
-        $entry->title = $request->title;
-        $entry->save();
-        return redirect('watchlist')->with('status', 'Your comment has been posted!');
+            ]);
+
+            $entry = new Entry;
+            $entry->movie_id_fk = $request->movie_id_fk;
+            $entry->watchlist_id_fk = $request->watchlist_id_fk;
+            $entry->title = $request->title;
+            $entry->save();
+            return redirect('watchlist')->with('status', 'Your comment has been posted!');
+        }
     }
 
     /**
