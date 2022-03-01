2de8b6143d4acd1da8352aed5b722888951060c2
Naelia Salas <naeliasalas@gmail.com>
2022-03-01 20:47:00 +0100

    no change just accidentaly saved

---
+++ project/imdbclone/resources/views/search-new.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search movies CINEMAH</title>
+    <link rel="stylesheet" href="{{ asset('')}}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+    <link rel="stylesheet" href="{{url('css/hero.css')}}" />
+    <link rel="stylesheet" href="{{url('css/welcome.css')}}" />
+    <link rel="stylesheet" href="{{url('css/footer.css')}}" />
+    <link rel="stylesheet" href="{{url('css/highlight.css')}}" />
+    <link rel="stylesheet" href="{{url('css/watchlist.css')}}" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/405a204514.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <title>CINEMAH</title>
+</head>
+
+<body>
+
+
+
+
+</body>
