26c4258b3a32061d8ad18e7b06ff6b20515cca99
Nova Boman <nova.boman@chasacademy.se>
2022-03-01 08:45:48 +0100

    Change UserController to resource controller

--- project/imdbclone/app/Http/Controllers/UserController.php
+++ project/imdbclone/app/Http/Controllers/UserController.php
@@ -7,48 +7,78 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-
-        $users = User::get();
-        return view('admin.user', ['users' => $users]);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
-    //edit update
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
-
-        $users = User::find($id);
-        return view('admin.edit', ['users' => $users]);
+        //
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
-
     {
-
-        $request->validate([
-            'name' => 'string|required|max:255',
-            'email' => 'required|email|',
-            'username' => 'string|required|max:255',
-
-        ]);
-
-        $user = User::find($id);
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->username = $request->username;
-        $user->update();
-
-        return redirect('/user')->with('status', 'The user has been updated!');
+        //
     }
 
-    //delete
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
-        $users = User::find($id);
-        $users->delete();
-        return back()->with('status', 'The user has been deleted!');
+        //
     }
 }
