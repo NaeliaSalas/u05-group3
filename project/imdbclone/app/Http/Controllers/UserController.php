b20a1b25467cbf700573ee11c3bdf2eeb13e9940
Nova Boman <nova.boman@chasacademy.se>
2022-03-01 10:57:48 +0100

    Fix redirect in destroy function

--- project/imdbclone/app/Http/Controllers/UserController.php
+++ project/imdbclone/app/Http/Controllers/UserController.php
@@ -96,6 +96,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect('user');
     }
 }
