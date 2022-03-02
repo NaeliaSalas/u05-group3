cce122e8d2c09ea2838923eb1832bdc91251473b
Nova Boman <nova.boman@chasacademy.se>
2022-03-02 13:02:32 +0100

    Add create function, connect to and update addmovie.blade

--- project/imdbclone/resources/views/admin/movies.blade.php
+++ project/imdbclone/resources/views/admin/movies.blade.php
@@ -89,7 +89,7 @@
         </div>
 
         <div class="w-full h-full p-4 m-8 overflow-y-auto">
-            <a href="add-movie.html">
+            <a href="/movie/create">
                 <button type="button" class="justify-right px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Add movie +</button>
             </a>
             <!-- Content-->
