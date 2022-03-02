785c20c4f95db637b381c71862db7fd9bcf97e1e
David Melander <david.melander@chasacademy.se>
2022-03-02 23:02:59 +0100

    123

--- project/imdbclone/resources/views/welcome.blade.php
+++ project/imdbclone/resources/views/welcome.blade.php
@@ -31,37 +31,36 @@
                         </a>
                     </div>
                 </nav>
-            <form action="{{ URL('/search')}}" method="GET">
-                @csrf
-                <div class="searchbar">
-                <input type="text" placeholder="Search Movie" name="search">
-                </div>
-            </form>
-
-            <div class="search-input">
-                <nav class="nav-wrapper">
-                    <ul class="nav-links">
-                        <li class="nav-item"><a class="menuItem" href="/">Home</a></li>
-                        <li class="nav-item"><a class="menuItem" href="#">Watchlist</a></li>
-                        <li class="nav-item"><a class="menuItem" href="category">Categories</a></li>
-                        @guest
-                        <li class=" nav-item"><a class="menuItem" href="login">Log in</a></li>
-                        <li class=" nav-item"><a class="menuItem" href="register">Register</a></li>
-                        @endguest
-                        @auth
-                        <li class=" nav-item"><a class="menuItem" href="/logout">Logout</a></li>
-                        @endauth
-                    </ul>
-                    <div class="hamburger">
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                        <span class="bar"></span>
+                <form action="{{ URL('/search')}}" method="GET">
+                    @csrf
+                    <div class="searchbar">
+                        <input type="text" placeholder="Search Movie" name="search">
                     </div>
-                </nav>
-             
+                </form>
+
+                <div class="search-input">
+                    <nav class="nav-wrapper">
+                        <ul class="nav-links">
+                            <li class="nav-item"><a class="menuItem" href="/">Home</a></li>
+                            <li class="nav-item"><a class="menuItem" href="#">Watchlist</a></li>
+                            <li class="nav-item"><a class="menuItem" href="category">Categories</a></li>
+                            @guest
+                            <li class=" nav-item"><a class="menuItem" href="login">Log in</a></li>
+                            <li class=" nav-item"><a class="menuItem" href="register">Register</a></li>
+                            @endguest
+                            @auth
+                            <li class=" nav-item"><a class="menuItem" href="/logout">Logout</a></li>
+                            @endauth
+                        </ul>
+                        <div class="hamburger">
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                        </div>
+                    </nav>
+                </div>
             </div>
         </div>
-    </div>
 
 
         <!-- hero slider section -->
