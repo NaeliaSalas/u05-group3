5aa5def173686503dbbd88cead7f2ddda46ce9bf
Jonny Norberg <90459832+JonnyNorberg@users.noreply.github.com>
2022-03-04 18:10:32 +0100

    Updated welcome-blade
    Added css href

--- project/imdbclone/resources/views/welcome.blade.php
+++ project/imdbclone/resources/views/welcome.blade.php
@@ -6,11 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
-    <link rel="stylesheet" href="{{url('css/hero.css')}}" />
-    <link rel="stylesheet" href="{{url('css/welcome.css')}}" />
+    <link rel="stylesheet" href="{{url('css/button.css')}}" />
     <link rel="stylesheet" href="{{url('css/footer.css')}}" />
+    <link rel="stylesheet" href="{{url('css/hero.css')}}" />
     <link rel="stylesheet" href="{{url('css/highlight.css')}}" />
+    <link rel="stylesheet" href="{{url('css/modal.css')}}" />
+    <link rel="stylesheet" href="{{url('css/movie.css')}}" />
+    <link rel="stylesheet" href="{{url('css/review.css')}}" />
     <link rel="stylesheet" href="{{url('css/watchlist.css')}}" />
+    <link rel="stylesheet" href="{{url('css/welcome.css')}}" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://kit.fontawesome.com/405a204514.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
