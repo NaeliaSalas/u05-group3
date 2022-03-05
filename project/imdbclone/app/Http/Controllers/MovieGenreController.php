dfc24d3f06414d204c3a7ac3c5eb09985f8cb20a
Naelia Salas <naeliasalas@gmail.com>
2022-03-05 16:08:37 +0100

    Add some Java script to the form action

--- project/imdbclone/app/Http/Controllers/MovieGenreController.php
+++ project/imdbclone/app/Http/Controllers/MovieGenreController.php
@@ -2,20 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Genre;
 use App\Models\Movie;
 use App\Models\MovieGenres;
 use Illuminate\Http\Request;
 
 class MovieGenreController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
 
 
         $genres = MovieGenres::get('genre_id_fk');
-      
-        
-        return view('entry.show', ['genres' => $genres]);
 
+
+        return view('entry.show', ['genres' => $genres]);
+    }
+
+    public function show($genrename)
+    {
+        $genre = Genre::where('genre', $genrename)->first();
+        return view('genre.genre', ['genre' => $genre]);
+    }
+
+    public function movieGenresId($id)
+    {
+
+        $moviegenreid = MovieGenres::get($id);
+        return view('movie.create', ['moviegenreid' => $moviegenreid]);
     }
 }
