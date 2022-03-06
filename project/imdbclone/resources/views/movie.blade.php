edc01f43ab6d0fc2a74e14df4bb25aa2bd17ee3c
David Melander <david.melander@chasacademy.se>
2022-03-06 12:26:00 +0100

    styling

--- project/imdbclone/resources/views/movie.blade.php
+++ project/imdbclone/resources/views/movie.blade.php
@@ -85,175 +85,174 @@
             <div class="movie-hero">
                 <img src="{{url($movie->hero)}}" alt="" class="">
             </div>
+
             <div class="movieflex">
-                <<<<<<< HEAD <div class="flex-item">
+                <div class="flex-item">
                     <img src="{{url($movie->hero)}}" alt="" class="cover">
-            </div>
-            =======
-            >>>>>>> main
-
-            <!-- center space-->
-            <div class="flex-item"></div>
-
-            <!-- play-->
-            <div class="flex-item">
-                <button id="myBtn"><img src="{{ URL('images/PlayBtn.png')}}" alt="">l</button>
-                <h3>Spela trailer</h3>
-                <p>2021, 2h 32min - Drama, Historia, Action. Land: USA.</p>
-            </div>
-            <!-- play-->
-
-            <!--MODAL -->
-            <div id="myModal" class="modal">
-
-                <!-- Modal content -->
-                <div class="modal-content">
-                    <span class="close">&times;</span>
-                    <iframe src="{{url($movie->trailer)}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
+
+                <!-- center space-->
+                <div class="flex-item"></div>
+
+                <!-- play-->
+                <div class="flex-item">
+                    <button id="myBtn"><img src="{{ URL('images/PlayBtn.png')}}" alt="">l</button>
+                    <h3>Spela trailer</h3>
+                    <p>2021, 2h 32min - Drama, Historia, Action. Land: USA.</p>
+                </div>
+                <!-- play-->
+
+                <!--MODAL -->
+                <div id="myModal" class="modal">
+
+                    <!-- Modal content -->
+                    <div class="modal-content">
+                        <span class="close">&times;</span>
+                        <iframe src="{{url($movie->trailer)}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <!--MODAL -->
+
             </div>
-            <!--MODAL -->
 
         </div>
+        <!-- Movie hero -->
 
-    </div>
-    <!-- Movie hero -->
-
-    <!-- txt section -->
-    <div class="main">
-        <div class="txtSection">
-            <h1 class="MovieH1">{{$movie->title}}</h1>
-            <p class="MovieTxt"> {{$movie->body}}</p>
-            <br>
-            <p class="MovieTxt">Director: {{$movie->director}}</p>
-            <p class="MovieTxt">Year: {{$movie->yearproduced}}</p>
-            <p class="MovieTxt">Rating: {{$movie->rating}}</p>
+        <!-- txt section -->
+        <div class="main">
+            <div class="txtSection">
+                <h1 class="MovieH1">{{$movie->title}}</h1>
+                <p class="MovieTxt"> {{$movie->body}}</p>
+                <br>
+                <p class="MovieTxt">Director: {{$movie->director}}</p>
+                <p class="MovieTxt">Year: {{$movie->yearproduced}}</p>
+                <p class="MovieTxt">Rating: {{$movie->rating}}</p>
 
 
-        </div>
+            </div>
 
-        <div class="dropdown">
-            <a href="/register"><button class="dropbtn btn-17">Add to watchlist</button></a>
+            <div class="dropdown">
+                <a href="/register"><button class="dropbtn btn-17">Add to watchlist</button></a>
+                @auth
+                <div class="dropdown-content" style="left:0;">
+                    @foreach(Auth::user()->watchlists as $watchlist)
+                    <a href="#">{{$watchlist->title}}</a>
+                    @endforeach
+                </div>
+                @endauth
+            </div>
+
+            <!-- Add reviews -->
+            <div class="center">
+                <button class="custom-btn btn-15">Add review</button>
+            </div>
+            <!-- Add reviews -->
+
+            <!-- images section -->
+            <section class="featured">
+                <h2 class="reviewstext"><a href="#">User reviews</a></h2>
+            </section>
+            <!-- reviews section -->
+            <div class="reviewFlex">
+                @if ($movie->reviews->isEmpty())
+                <p class="MovieTxt">
+                    Be the first to review this!
+                </p>
+                @else
+                @foreach ($movie->reviews as $review)
+                <div class="review-item">
+                    <p class="MovieTxt">{{$review->user->username}}<br>
+                        {{ $review->title }}
+                        @for ($i = 0; $i < $movie->rating; $i++)
+                            <i class="fa-solid fa-star"></i>
+                            @endfor
+
+                    </p>
+                    <br>
+                    <p>{{ $review->body }}</p>
+                </div>
+                @endforeach
+                @endif
+            </div>
+            <!-- reviews section -->
+
+            <!-- Add reviews -->
+            <div class="center">
+                <a href="{{'/movie/' . $movie->id . '/review/create'}}"><button class="custom-btn btn-15">Add review</button></a>
+            </div>
+            <!-- Add reviews -->
+
+            <!-- Your watchlist section -->
             @auth
-            <div class="dropdown-content" style="left:0;">
+
+            <section class="featured">
+                <h2><a href="/watchlist">Your watchlist > </a></h2>
+            </section>
+
+            <div class="watchlist">
                 @foreach(Auth::user()->watchlists as $watchlist)
-                <a href="#">{{$watchlist->title}}</a>
+                @foreach($watchlist->movies as $movie)
+                <div class="highlight_item">
+                    <a href="url('/movie/' . $movie->id)"><img src="{{ URL($movie->cover)}}" alt="Movie cover"></a>
+                    <div class="button_border">
+                        <p>{{ $movie->title }}</p>
+                        <div class="rating">
+                            <i class="fa-solid fa-star"></i>
+                            <p>{{ $movie->rating }}</p>
+                        </div>
+
+                    </div>
+                </div>
+                @endforeach
                 @endforeach
             </div>
             @endauth
+
+
         </div>
 
-        <!-- Add reviews -->
-        <div class="center">
-            <button class="custom-btn btn-15">Add review</button>
-        </div>
-        <!-- Add reviews -->
-
-        <!-- images section -->
-        <section class="featured">
-            <h2 class="reviewstext"><a href="#">User reviews</a></h2>
-        </section>
-        <!-- reviews section -->
-        <div class="reviewFlex">
-            @if ($movie->reviews->isEmpty())
-            <p class="MovieTxt">
-                Be the first to review this!
-            </p>
-            @else
-            @foreach ($movie->reviews as $review)
-            <div class="review-item">
-                <p class="MovieTxt">{{$review->user->username}}<br>
-                    {{ $review->title }}
-                    @for ($i = 0; $i < $movie->rating; $i++)
-                        <i class="fa-solid fa-star"></i>
-                        @endfor
-
-                </p>
-                <br>
-                <p>{{ $review->body }}</p>
-            </div>
-            @endforeach
-            @endif
-        </div>
-        <!-- reviews section -->
-
-        <!-- Add reviews -->
-        <div class="center">
-            <a href="{{'/movie/' . $movie->id . '/review/create'}}"><button class="custom-btn btn-15">Add review</button></a>
-        </div>
-        <!-- Add reviews -->
-
-        <!-- Your watchlist section -->
-        @auth
-
-        <section class="featured">
-            <h2><a href="/watchlist">Your watchlist > </a></h2>
-        </section>
-
-        <div class="watchlist">
-            @foreach(Auth::user()->watchlists as $watchlist)
-            @foreach($watchlist->movies as $movie)
-            <div class="highlight_item">
-                <a href="url('/movie/' . $movie->id)"><img src="{{ URL($movie->cover)}}" alt="Movie cover"></a>
-                <div class="button_border">
-                    <p>{{ $movie->title }}</p>
-                    <div class="rating">
-                        <i class="fa-solid fa-star"></i>
-                        <p>{{ $movie->rating }}</p>
+        <!-- Footer with links -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col">
+                        <h4>company</h4>
+                        <ul>
+                            <li><a href="#">about us</a></li>
+                            <li><a href="#">Condition of Use</a></li>
+                            <li><a href="#">privacy policy</a></li>
+                            <li><a href="#">Site Index</a></li>
+                        </ul>
                     </div>
-
-                </div>
-            </div>
-            @endforeach
-            @endforeach
-        </div>
-        @endauth
-
-
-    </div>
-
-    <!-- Footer with links -->
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="footer-col">
-                    <h4>company</h4>
-                    <ul>
-                        <li><a href="#">about us</a></li>
-                        <li><a href="#">Condition of Use</a></li>
-                        <li><a href="#">privacy policy</a></li>
-                        <li><a href="#">Site Index</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>get help</h4>
-                    <ul>
-                        <li><a href="#">Help</a></li>
-                        <li><a href="#">Get the APP</a></li>
-                        <li><a href="#">CinemahPro</a></li>
-                        <li><a href="#">MojoIMDB Developer</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>market related</h4>
-                    <ul>
-                        <li><a href="#">Press Room</a></li>
-                        <li><a href="#">Box Office</a></li>
-                        <li><a href="#">Interest-Based Ads</a></li>
-                        <li><a href="#">Advertising</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>follow us</h4>
-                    <div class="social-links">
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    <div class="footer-col">
+                        <h4>get help</h4>
+                        <ul>
+                            <li><a href="#">Help</a></li>
+                            <li><a href="#">Get the APP</a></li>
+                            <li><a href="#">CinemahPro</a></li>
+                            <li><a href="#">MojoIMDB Developer</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>market related</h4>
+                        <ul>
+                            <li><a href="#">Press Room</a></li>
+                            <li><a href="#">Box Office</a></li>
+                            <li><a href="#">Interest-Based Ads</a></li>
+                            <li><a href="#">Advertising</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>follow us</h4>
+                        <div class="social-links">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
 
     </div>
 
